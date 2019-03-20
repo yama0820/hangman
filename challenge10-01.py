7d3059e04068317e35e322a8765956cf4acbc0ed
山田優介 <yusuke@Yusuke-MacBookPro.local>
2019-03-20 23:14:10 +0900

    my first commit

---
+++ challenge10-01.py
@@ -0,0 +1,43 @@
+import random
+
+def hangman():
+    word_list = ["Python","Java","computer","hacker","painter"]
+    random_number = random.randint(0,4)
+    word = word_list[random_number]
+    wrong = 0
+    stages = ["",
+              "____________",
+              "|           ",
+              "|     |     ",
+              "|     0     ",
+              "|    /|\    ",
+              "|    / \    ",
+              "|           ",
+              ]
+    rletters = list(word)
+    board = ["_"] * len(word)
+    win = False
+    print("ハングマンへようこそ！")
+    while wrong < len(stages) - 1:
+        print("\n")
+        msg = "1文字を予想してね"
+        char = input(msg)
+        if char in rletters:
+            cind = rletters.index(char)
+            board[cind] = char
+            rletters[cind] = '$'
+        else:
+            wrong += 1
+        print(" ".join(board))
+        e = wrong +1
+        print("\n".join(stages[0:e]))
+        if "_" not in board:
+            print("あなたの勝ち！")
+            print(" ".join(board))
+            win = True
+            break
+    if not win:
+        print("\n".join(stages[0:wrong+1]))
+        print("あなたの負け!正解は{}.".format(word))
+
+hangman()
